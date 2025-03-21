55f0a3b85e735c527c184751415bb1e973bea7e3
shivamkgn99550@gmail.com <shivamkgn99550@gmail.com>
2025-03-21 12:12:38 +0530

    add module

---
+++ subtract.py
@@ -0,0 +1,7 @@
+def subtract(a, b):
+    return a - b
+
+if __name__ == "__main__":
+    num1 = float(input("Enter first number: "))
+    num2 = float(input("Enter second number: "))
+    print(f"Result: {subtract(num1, num2)}")
